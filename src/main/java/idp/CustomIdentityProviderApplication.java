55dfe49909b5dbafb1d94e62c5758c0007b0f50e
Kartik Sarraf <kartiksarraf786@gmail.com>
2022-06-23 21:49:31 +0530

    Added war packaging

--- src/main/java/idp/CustomIdentityProviderApplication.java
+++ src/main/java/idp/CustomIdentityProviderApplication.java
@@ -2,9 +2,10 @@ package idp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication()
-public class CustomIdentityProviderApplication {
+public class CustomIdentityProviderApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(CustomIdentityProviderApplication.class, args);
